87db6fd2281a3e56ea07d15e2e6379f0a1ad0cab
firemodz <32964018+firemodz@users.noreply.github.com>
2017-10-20 19:35:06 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,99 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+bot.on('ready', function(){
+  console.log('Ready!');
+});
+bot.on('message', (message) =>  {
+    if(message.content == 'nyan cat 4 liffffffff') {
+        message.channel.send('nyan cat11');
+    }
+      if(message.content == 'nyan joke') {
+        message.channel.send('nyan hav cool pens in school');
+    }
+      if(message.content == 'nyan cat die') {
+        message.channel.send('no');
+    }
+      if(message.content == 'nyan cat eat poop') {
+        message.channel.send('mistaken drink poop');
+    }
+      if(message.content == 'nyan cat is cool') {
+        message.channel.send('`yes`');
+    }
+      if(message.content == 'nyan cat wut r u') {
+        message.channel.send('`cat with body of toast and jam on with rainbow trail`');
+    }
+      if(message.content == 'nyan cat dm me') {
+        message.author.send('`dmed ok`');
+    }
+      if(message.content == 'nyan cat imageme') {
+        message.channel.sendFile('https://cdn.discordapp.com/attachments/352586720983646218/370652405010006021/Nyan_Cat.jpeg');
+    }
+      if(message.content == 'nyan cat warn') {
+        message.author.send('`u have been warned`');
+        message.channel.send('`u been warned lol`');
+    }
+      if(message.content == 'nyan cat abc') {
+        message.author.send('`abcdefgjklmnopqrstuvwsyz`');
+        message.channel.send('`abcdefgjklmnopqrstuvwsyz`');
+    }
+      if(message.author.send == 'nyan cat 4 liffffffff') {
+        message.channel.send('nyan cat11');
+    }
+      if(message.author.send == 'nyan joke') {
+        message.channel.send('nyan hav cool pens in school');
+    }
+      if(message.author.send == 'nyan cat die') {
+        message.channel.send('no');
+    }
+      if(message.author.send == 'nyan cat eat poop') {
+        message.channel.send('mistaken drink poop');
+    }
+      if(message.author.send == 'nyan cat is cool') {
+        message.channel.send('`yes`');
+    }
+      if(message.content == 'nyan cat wut r u') {
+        message.channel.send('`cat with body of toast and jam on with rainbow trail`');
+    }
+      if(message.author.send == 'nyan cat dm me') {
+        message.author.send('`dmed ok`');
+    }
+      if(message.author.send == 'nyan cat imageme') {
+        message.channel.sendFile('https://cdn.discordapp.com/attachments/352586720983646218/370652405010006021/Nyan_Cat.jpeg');
+    }
+      if(message.author.send == 'nyan cat warn') {
+        message.author.send('`u have been warned`');
+        message.channel.send('`u been warned lol`');
+    }
+      if(message.author.send == 'nyan cat abc') {
+        message.author.send('`abcdefgjklmnopqrstuvwsyz`');
+        message.channel.send('`abcdefgjklmnopqrstuvwsyz`');
+    }
+     if(message.author.send == "nc/help"){
+       message.author.send('`- nyan cat 4 liffffffff`');
+       message.author.send('`- nyan joke`');
+       message.author.send('`- nyan cat die`');
+       message.author.send('`- nyan cat eat poop`');
+       message.author.send('`- nyan cat is cool`');
+       message.author.send('`- nyan cat wut r u`');
+       message.author.send('`- nyan cat dm me`');
+       message.author.send('`- nyan cat imageme`');
+       message.author.send('`- nyan cat warn`');
+       message.author.send('`- nyan cat abc`');
+     }
+if(message.content == "nc/help"){
+       message.channel.send('`Commands`');
+       message.channel.send('`None needed prefix commands`');
+       message.channel.send('`- nyan cat 4 liffffffff`');
+       message.channel.send('`- nyan joke`');
+       message.channel.send('`- nyan cat die`');
+       message.channel.send('`- nyan cat eat poop`');
+       message.channel.send('`- nyan cat is cool`');
+       message.channel.send('`- nyan cat wut r u`');
+       message.channel.send('`- nyan cat dm me`');
+       message.channel.send('`- nyan cat imageme`');
+       message.channel.send('`- nyan cat warn`');
+       message.channel.send('`- nyan cat abc`');
+     }
+});
+
+bot.login('MzcwNjQ4OTAzOTgyMzgzMTA1.DMqJNw.aKwRpkCY9lZz7Aj2Ewf6zKLWr1A');
