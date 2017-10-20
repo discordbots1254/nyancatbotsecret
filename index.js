a3d6a395d540f464aa8d22d3dfe390a072478c6f
firemodz <32964018+firemodz@users.noreply.github.com>
2017-10-20 19:44:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-bot.on('ready', function(){
-  console.log('Ready!');
-});
 bot.on('message', (message) =>  {
     if(message.content == 'nyan cat 4 liffffffff') {
         message.channel.send('nyan cat11');
